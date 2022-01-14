7d6479e27fdafb3c47f31d8c5da725a1e14a34b5
Alex Nijjar <61668546+AlexNijjar@users.noreply.github.com>
2022-01-14 05:57:55 -0700

    Tested with other mods

--- src/main/java/com/github/alexnijjar/the_extractinator/config/SupportedModsConfig.java
+++ src/main/java/com/github/alexnijjar/the_extractinator/config/SupportedModsConfig.java
@@ -2,13 +2,19 @@ package com.github.alexnijjar.the_extractinator.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "supported_mods")
 public class SupportedModsConfig implements ConfigData {
 
+    @ConfigEntry.Gui.RequiresRestart
     public boolean minecraft_support = true;
+    @ConfigEntry.Gui.RequiresRestart
     public boolean modern_industrialization_support = true;
+    @ConfigEntry.Gui.RequiresRestart
     public boolean techreborn_support = true;
+    @ConfigEntry.Gui.RequiresRestart
     public boolean indrev_support = true;
+    @ConfigEntry.Gui.RequiresRestart
     public boolean ae2_support = true;
 }
